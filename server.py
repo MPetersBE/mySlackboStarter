065c7a737b8d5082cffef5b2d7bceb9937f58214
MPetersBE <marc.peters@de.ibm.com>
2017-03-28 16:33:21 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-########
+#author mpetersbe - inspired by niyatip
+from __future__ import print_function
 from slackclient import SlackClient
 from watson_developer_cloud import ConversationV1
 
@@ -12,48 +13,64 @@ BOT_ID = os.environ.get("BOT_ID")
 AT_BOT = "<@" + BOT_ID + ">"
 
 # instantiate Slack clients
-sc = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 #instantiate workspace and context for Conversation service
 WORKSPACE_ID = os.environ.get("WORKSPACE_ID")
 USERNAME = os.environ.get("WORKSPACE_USERNAME")
 PASSWORD = os.environ.get("WORKSPACE_PASSWORD")
-
-CHANNEL_NAME = "energybot"
+context = {}
 
 #Link to Watson Conversation as Auth is completed
-# Replace with your own service credentials
 conversation = ConversationV1(
-username= USERNAME,
-password= PASSWORD,
-version='2017.03.27')
+    username= USERNAME,
+    password= PASSWORD,
+    version='2016-09-20'
+)
+    
+def handle_command(command, channel, user):
 
-def main():
-    # Connect to slack
-    if sc.rtm_connect():
-        # Send first message
-        sc.rtm_send_message(CHANNEL_NAME, "Hello! I am here to help yopu!")
+  attachments = ""
 
+  #Get response from Watson Conversation
+  responseFromWatson = conversation.message(
+      workspace_id=WORKSPACE_ID,
+      message_input={'text': command},
+      context=context
+  )
+
+  #Get intent of the query
+  response = responseFromWatson['output']['text'][0]
+        
+  slack_client.api_call("chat.postMessage", as_user=True, channel=channel, text=response,
+                    attachments=attachments)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        This parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip(), \
+                       output['channel'], output['user']
+    return None, None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
         while True:
-            # Read latest messages
-            for slack_message in sc.rtm_read():
-                message = slack_message.get("text")
-                user = slack_message.get("user")
-                if not message or not user:
-                    continue
-                #Get response from Watson Conversation
-                responseFromWatson = conversation.message(
-                    workspace_id=WORKSPACE_ID,
-                    message_input={'text': command},
-                    context=context
-                response = responseFromWatson['output']['text'][0] 
-                sc.api_call("chat.postMessage", as_user=True, channel=channel, text=response,
-                      attachments=attachments)
-                sc.rtm_send_message(CHANNEL_NAME, "<@{}> wrote something...".format(user))
-            # Sleep for half a second
-            time.sleep(0.5)
+            command, channel, user = parse_slack_output(slack_client.rtm_read())
+            if command and channel and user:
+                handle_command(command, channel, user)
+            time.sleep(READ_WEBSOCKET_DELAY)
     else:
-        print("Couldn't connect to slack")
+        print("Connection failed. Invalid Slack token or bot ID?")
 
-if __name__ == '__main__':
-    main()
